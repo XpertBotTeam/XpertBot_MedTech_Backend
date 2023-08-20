be0c012e3bb7e30646edf50128690539c1b5846b
AshrafSharafCS <ashrafsharafcs@gmail.com>
2023-08-21 02:31:27 +0300

    implement api registration and authentication

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+
+    public function login(Request $request)
+    {
+        $credentials = $request->only(['email', 'password']);
+
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+            $access_token = $user()->createToken('authtoken')->plainTextToken;
+
+            return response()->json([
+                'success' => true,
+                'token' => $access_token,
+                'message' => 'User logged-in successfully'
+            ]);
+        } else {
+            return response()->json([
+                'success' => false,
+                'message' => 'Wrong username or password'
+            ]);
+        }
+    }
+
+
+    public function register(Request $request)
+    {
+       
+        $user = User::where('email', $request->email)->first();
+        if (is_null($user)) {
+            $user = new User();
+            $user->name = $request->name;
+            $user->email = $request->email;
+            $user->password = bcrypt($request->password);
+            $user->save();
+
+            $token = $user->createToken('authtoken')->plainTextToken;
+
+            return response()->json([
+                'status' => true,
+                'message' => 'User Created Successfully',
+                'token' => $token
+            ]);
+        } else {
+            return response()->json([
+                'status' => false,
+                'message' => 'User already exists'
+            ]);
+        }
+    }
+}
